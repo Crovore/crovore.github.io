ae7d0b1f489101e84037202410369291700e2c90
Crovore <146949587+Crovore@users.noreply.github.com>
2024-03-23 22:43:01 +0100

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,118 @@
+const VIDEO = {
+    "burton": {
+      "role": "Monteur, Acteur",
+      "titre": "Tim Burton Review",
+      "description": "Cette vidéo a été réalisée dans le cadre d'un projet d'étude pour lequel il fallait réaliser une vidéo sur un sujet cinématographique libre. Avec mon groupe de travail, nous avons choisi le réalisateur Tim Burton.",
+      "competences": {
+        "VFX": "Amélioration de mes compétences avec After Effects pour l'effet de portail",
+        "Décor": "Minutie dans chaque détails d'un décor et gestion de l'éclairage",
+        "Montage": "Dynamisme des plans pour un projet à caméra fixe",
+        "Sound design": "Ajout d'effets sonores pertinents et correctements mixés",
+        "Anglais" : "Maitrise fluide de la langue et de la communication non verbale pour faire oublier la présence d'un prompteur.",
+      },
+      "code": '<iframe width="560" height="315" src="https://www.youtube.com/embed/D2nbqxjNDzk?si=tEgUJDP3wIMO4Z0H" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+      "année": 2024,
+    },
+    
+    "motiondesign": {
+        "role": "Motion Designer, Sound Designer",
+        "titre": "Animation Motion Design",
+        "description": "Une très courte animation d'une illustration trouvée sur freepik.com, avec After effects, pour apprendre les bases du Motion Design." ,
+        "competences": {
+          "Motion design": "Vélocité des images-clés, décalage des actions entre les calques similaires, maitrise de la physique des calques",
+          "Sound design": "Chaque action doit être bruitée pour accentuer l'immersion et éviter un sentiment de vide.",
+        },
+        "code": '<iframe width="560" height="315" src="https://youtube.com/embed/VH_G3c0DC8g?si=VuzoaPfTP-5mR0Z_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+        "année": 2024,
+      },
+  
+    "gobeyond": {
+        "role": "Monteur, Acteur",
+        "titre": "Go Beyond",
+        "description": "Cette vidéo a été réalisée dans le cadre d'un projet d'étude et consistait en la réalisation d'une vidéo à impact positif. Le scénario prend l'histoire d'un jeune homme qui, après une blessure qui le plonge dans une profonde depression, remonte la pente et passe outre, d'où le titre du film.",
+        "competences": {
+          "VFX": "Utilisation de masques pour ajuster la luminosité des éléments de décor lors ddu cycle jour/nuit",
+          "Découpage technique": "Travail dans l'enchainement des plans pour une cohérence visuelle globale",
+          "Montage": "Dynamisme des transitions et première expérience d'étalonnage à partir d'une vidéo tournée en V-LOG",
+        },
+        "code": '<iframe width="560" height="315" src="https://www.youtube.com/embed/WVlj2WlJNz8?si=rSBVziNC8xWT5mtb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+        "année": 2023,
+      },
+      
+      "messe": {
+        "role": "Cadreur, Monteur",
+        "titre": "La Messe du Roi déchu",
+        "description": "Un court-métrage historique relatant les derniers instants du Roi Louis XVI, réalisé dans le cadre du film de fin d'étude en option Cinéma-Audiovisuel" ,
+        "competences": {
+          "Étalonnage": "Travailler dans les teintes orangées pour faire ressortir l'ambiance de l'époque",
+          "Montage": "Donner une atmosphère pesante sur le spectateur grâce au sound design.",
+        },
+        "code": '<iframe width="560" height="315" src="https://youtube.com/embed/ZYQ_J9N3iVI?si=9_ZaAaKZS3kpwtsT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+        "année": 2023,
+      },
+      
+      "allemand": {
+        "role": "Cadreur, Monteur, Acteur",
+        "titre": "Vidéo de présentation en allemand",
+        "description": "Une vidéo de présentation en allemand dans laquelle je me présente de manière très classique. Le focus ici est porté sur le décor, le dynamisme et les effets." ,
+        "competences": {
+          "Montage": "Dynamisme des plans pour un projet à caméra fixe",
+          "Décor": "Mise en place harmonieuse des éléments et gestion de l'éclairage",
+          "Effets": "Utilisation des outils de tracking d'after effects pour le suivi de mouvement du visage ou le tracking d'un texte sur un objet.",
+        },
+        "code": '<iframe width="560" height="315" src="https://youtube.com/embed/0Hl6AAgWlik?si=Ys9_sQsIpCauIrZl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+        "année": 2024,
+      },
+
+      "interview": {
+        "role": "Monteur, Acteur",
+        "titre": "Interview type Konbini",
+        "description": "Cette courte vidéo, en lien avec le projet " + '"Go Beyond"' + " fût un exercice de reprise des codes de l'interview, demandant un montage dynamique, du sous-titrage et des animations 2D." ,
+        "competences": {
+          "Motion design": "Première expérience en animation 2D sur After Effects, pour les titres d'introduction et de questions",
+          "Cadrage": "Apprentissage du triangle d'exposition sur un plan fixe pour obtenir un flou d'arrière-plan de qualité.",
+          "Montage": "Dynamisme dans l'enchainement des plans pour ne laisser aucun blanc dans les phrases",
+        },
+        "code": '<iframe width="560" height="315" src="https://www.youtube.com/embed/kDlHTaO_i1k?si=Zh7z5c1qNdiSyq4K" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+        "année": 2023,
+      },
+
+      "retrocamps": {
+        "role": "Cadreur, Monteur",
+        "titre": "Retrocamps scout 2023",
+        "description": "Cette vidéo est une rétrospective du camps scout de l'été 2023 que j'ai vécu avec mon équipe. Notre mission principale était d'aménager un espace extérieur dans une ferme coopérative qui nous accueillait pour deux semaines." ,
+        "competences": {
+          "Derush": "Sélection des plans pertinents parmi plus de 850 fichiers",
+          "Montage": "Cohésion des plans avec la musique",
+        },
+        "code": '<iframe width="560" height="315" src="https://www.youtube.com/embed/6EdFBLP0XG8?si=KNkCsI2fFQwnc569" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+        "année": 2023,
+      },
+
+      "vin": {
+        "role": "Monteur",
+        "titre": "Dégustation de vin",
+        "description": "Voici une vidéo parmi une quinzaine que j'ai montée pour des amis qui se lancaient dans la dégustation de vins sur YouTube et TikTok. Chaque vidéo YouTube était ensuite retravaillée au format vertical, adapté à TikTok." ,
+        "competences": {
+          "Montage": "Dynamisme des plans pour un projet à caméra fixe",
+        },
+        "code": '<iframe width="560" height="315" src="https://www.youtube.com/embed/8Nw57xEpIy4?si=Efb17KEJaGxZS0C1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+        "année": 2023,
+      },  
+      
+      "minecraft": {
+        "role": "Monteur",
+        "titre": "Vidéo Minecraft",
+        "description": "Cette vidéo retrace une aventure de plus de 300h sur le jeu vidéo Minecraft et présente une construction originale et créative dans un monde ou une seule mort me supprime l'accès au monde." ,
+        "competences": {
+          "Derush": "Sélection des plans pertinents pour retracer 300h en 5min de vidéo",
+          "Montage": "Cohérence entre la narration et les images montrées",
+          "Créativité": "Et oui, en dehors de la vidéo en elle-même, ce projet m'a demandé une certaine inventivité!",
+          "Anglais": "J'ai décidé de réaliser ce projet en anglais, pour toucher un public plus large. Ce fût un bon exercice."
+        },
+        "code": '<iframe width="560" height="315" src="https://www.youtube.com/embed/clcFfsE_JoU?si=9Xr-CgXtEURgaPYp" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
+        "année": 2022,
+      },
+
+      
+  }
---
+++ js/dataapropos.js
@@ -0,0 +1,31 @@
+const CI = {
+    "Video": {
+      "année": "Depuis 2015",
+    },
+
+    "Scoutisme": {
+      "année": "Depuis 2013",
+    },
+
+    // "Parkour": {
+    //   "année": "2019-2023",
+    // },
+
+    "Street Workout": {
+      "année": "Depuis 2022",
+    },
+
+    "Escalade": {
+      "année": "Depuis 2021",
+    },
+
+    // "Arts du cirque": {
+    //   "année": "2011-2019",
+    // },
+
+    "Saxophone": {
+      "année": "2011-2021",
+    },
+  
+    
+  }
